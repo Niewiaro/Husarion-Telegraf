441eafe013e414610e4ca93fc8ae5070e5aff5b0
Jakub <56446320+Niewiaro@users.noreply.github.com>
2024-10-31 14:34:26 +0100

    chore: binary to decimal

--- main.cpp
+++ main.cpp
@@ -11,8 +11,24 @@ int input_wheel_offset = 10;
 int input_wheel_curent_state = 0;
 bool binary_array[4] = {1, 1, 1, 1};
 int binary_array_index = 0;
+int binary_array_size = 4;
+int number_output = 0;
 int input_wheel_encoder_dalay = 100;
 int input_wheel_home_position_delay = 1000;
+
+int binaryToDecimal(const bool *binary, int size)
+{
+	int decimal = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		decimal += binary[i] * (1 << (size - 1 - i)); // (1 << n) to przesunięcie bitowe, które działa jak 2^n
+	}
+	if (decimal > 9)
+	{
+		return 0;
+	}
+	return decimal;
+}
 
 void input_wheel_home_position()
 {
@@ -50,8 +66,10 @@ void input_wheel_encoder()
 			if (binary_array_index == 4)
 			{
 				Serial.printf("binary_array_index: %d\r\n", binary_array_index);
+				number_output = binaryToDecimal(binary_array, binary_array_size);
+				Serial.printf("number_output: %d\r\n", number_output);
 
-				for (int i = 0; i < 4; ++i)
+				for (int i = 0; i < binary_array_size; ++i)
 				{
 					binary_array[i] = 1;
 				}
@@ -68,7 +86,7 @@ void init()
 	input_wheel_offset = 10;
 	input_wheel_curent_state = input_wheel_start_state;
 
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < binary_array_size; ++i)
 	{
 		binary_array[i] = 1;
 	}
